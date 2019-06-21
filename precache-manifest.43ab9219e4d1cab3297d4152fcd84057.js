14841322e455dc5f5a0c2fb992edd28d8aa16e67
Julio Truzzi <julio.truzzi@gmail.com>
2019-06-21 08:35:30 -0300

    Updates

---
+++ precache-manifest.43ab9219e4d1cab3297d4152fcd84057.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3044caa9b4a3a0913212f7443f509797",
+    "url": "/marvel-superheroes-demo/index.html"
+  },
+  {
+    "revision": "0fc2b5af32def12e3132",
+    "url": "/marvel-superheroes-demo/static/js/2.b2e31b50.chunk.js"
+  },
+  {
+    "revision": "60bbce5372bde763cdf6",
+    "url": "/marvel-superheroes-demo/static/js/main.8d3920b7.chunk.js"
+  },
+  {
+    "revision": "ae58ff9afc5c0d253c5e",
+    "url": "/marvel-superheroes-demo/static/js/runtime~main.491b8370.js"
+  }
+]);
